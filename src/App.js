b1124cb9f189bfc081c2295c5e600d97fdd2f62e
Deivyd1995 <davideliasmoriello@gmail.com>
2022-03-31 19:47:59 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Botones from "./components/Botones/Botones";
+import Diseño from "./components/Diseño/Diseño";
+
+
+
+function App() {
+  return (
+    <div className="layout">
+      
+      <Diseño />
+
+    </div>
+  );
+}
+
+export default App;
